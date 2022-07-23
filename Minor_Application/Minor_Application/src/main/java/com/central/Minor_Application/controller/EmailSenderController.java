f502196014156606c77f085597beb1f308b747e2
Aditya Gupta <adityaagupta9961@gmail.com>
2022-07-23 19:36:28 +0530

    bsabbj

--- Minor_Application/Minor_Application/src/main/java/com/central/Minor_Application/controller/EmailSenderController.java
+++ Minor_Application/Minor_Application/src/main/java/com/central/Minor_Application/controller/EmailSenderController.java
@@ -20,8 +20,8 @@ public class EmailSenderController {
 	private EmailService emailService;
 	
 	//Declaring value of aditya
-	String aditya = "5474";
-	//above mentioned line made to use it in future
+	String aditya = "5474gdagdj";
+	//above mentioned line made to use it in future babjbnm
 	@PostMapping("/public/email")
 	public ResponseEntity<?> sendEmail() throws MessagingException {
 		boolean result = this.emailService.sendEmail();
